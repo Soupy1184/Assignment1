85ad6738c9c0a30fd9b062170e60ee938a7b2dda
christophercampbell <chris.campbell1184@gmail.com>
2021-01-25 22:12:47 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        
+    }
+
+    public static String encrypt(String key, String plaintext){
+        String cipherText = "";
+        String[] keyspace = new String[key.length()];
+        
+        return cipherText;
+    }
+}
